2e59022f6e8b901be3ef8e6324f8bd8aa24694cb
CJ Johnson <Fermentedbeats@gmail.com>
2015-07-06 20:41:55 -0700

    several menus completed

--- v2/menu2.js
+++ v2/menu2.js
@@ -1,10 +1,10 @@
 function vodApp() {
 
+    var module = module || {};
+
 // constructor for each movie
-function Media(boxCover, trailerUrl, movieUrl, searchName, year) {
+function Media(boxCover, searchName, year) {
     this.boxCover = boxCover;
-    this.trailerUrl = trailerUrl;
-    this.movieUrl = movieUrl;
     this.searchName = searchName;
     this.year = year;
     this.title = "";
@@ -13,16 +13,20 @@ function Media(boxCover, trailerUrl, movieUrl, searchName, year) {
     this.rated = "";
     this.imdbRating = "";
     this.runtime = "";
-    apiCall(this);
+    module.apiCall(this, module.init);
 }
 
-// favorite, watchlist & history default as false
+// defaults
 Media.prototype.favorite = false;
 Media.prototype.watchList = false;
 Media.prototype.history = false;
+Media.prototype.trailerUrl = "../assets/videos/movieclip.mp4"
+Media.prototype.videoUrl = "../assets/videos/movieclip.mp4"
 
+
+module = {
 // API CALL
-function apiCall(obj){
+apiCall: function(obj, callback){
     var link = "http://www.omdbapi.com/?t=" + obj.searchName + "&y=" + obj.year + "&plot=short&r=json";
     var oReq = new XMLHttpRequest();
     oReq.open("get", link, true);
@@ -36,44 +40,41 @@ function apiCall(obj){
         obj.rated = apiReturn.Rated;
         obj.imdbRating = apiReturn.imdbRating;
         obj.runtime = apiReturn.Runtime;
-        console.log(obj);
+        callback(module);
     }
-} // end of apiCall function
+}, // end of apiCall function
 
-var video = '../assets/videos/movieclip.mp4';
 
 // create movie objects
 // put movie objects into table
 
-function makeMovieObjects(callback) {
+makeMovieObjects: function() {
 
-    var table = [
-new Media('../assets/boxcovers/avengers.jpg', video, video, "Avengers", "2012"),
-new Media('../assets/boxcovers/blade_runner.jpg', video, video, "Blade Runner", "1982"),
-new Media('../assets/boxcovers/brave.jpg', video, video, "Brave", "2012"),
-new Media('../assets/boxcovers/catching_fire.jpg', video, video, "The Hunger Games: Catching Fire", "2013"),
-new Media('../assets/boxcovers/django.jpg', video, video, "Django Unchained", "2012"),
-new Media('../assets/boxcovers/finding_nemo.jpg', video, video, "Finding Nemo", "2003"),
-new Media('../assets/boxcovers/hobbit.jpg', video, video, "The Hobbit: An Unexpected Journey", "2012"),
-new Media('../assets/boxcovers/hotel_transylvania.jpg', video, video, "Hotel Transylvania", "2012"),
-new Media('../assets/boxcovers/hugo.jpg', video, video, "Hugo", "2011"),
-new Media('../assets/boxcovers/iron_man.jpg', video, video, "Iron Man", "2008"),
-new Media('../assets/boxcovers/les_mis.jpg', video, video, "Les Miserables", "2012"),
-new Media('../assets/boxcovers/lorax.jpg', video, video, "The Lorax", "2012"),
-new Media('../assets/boxcovers/man_of_steel_large.jpg', video, video, "Man of Steel", "2013"),
-new Media('../assets/boxcovers/monsters_inc.jpg', video, video, "Monsters Inc", "2001"),
-new Media('../assets/boxcovers/new_moon.jpg', video, video, "The Twilight Saga: New Moon", "2009"),
-new Media('../assets/boxcovers/simpsons_movie_large.jpg', video, video, "The Simpsons Movie", "2007"),
-new Media('../assets/boxcovers/skyfall_large.jpg', video, video, "Skyfall", "2012"),
-new Media('../assets/boxcovers/spider_man.jpg', video, video, "Spider-Man", "2012"),
-new Media('../assets/boxcovers/star_trek.jpg', video, video, "Star Trek", "2009"),
-new Media('../assets/boxcovers/train_dragon.jpg', video, video, "How to Train Your Dragon", "2010"),
-new Media('../assets/boxcovers/wall_e.jpg', video, video, "Wall-E", "2008"),
-new Media('../assets/boxcovers/wreck_it_ralph.jpg', video, video, "Wreck-It Ralph", "2012"),
-];
-callback;
-return table;
-} // end of makeMovieObjects function
+module.table = [
+    new Media('../assets/boxcovers/avengers.jpg', "Avengers", "2012"),
+    new Media('../assets/boxcovers/blade_runner.jpg', "Blade Runner", "1982"),
+    new Media('../assets/boxcovers/brave.jpg', "Brave", "2012"),
+    new Media('../assets/boxcovers/catching_fire.jpg', "The Hunger Games: Catching Fire", "2013"),
+// new Media('../assets/boxcovers/django.jpg', "Django Unchained", "2012"),
+// new Media('../assets/boxcovers/finding_nemo.jpg', "Finding Nemo", "2003"),
+// new Media('../assets/boxcovers/hobbit.jpg', "The Hobbit: An Unexpected Journey", "2012"),
+// new Media('../assets/boxcovers/hotel_transylvania.jpg', "Hotel Transylvania", "2012"),
+// new Media('../assets/boxcovers/hugo.jpg', "Hugo", "2011"),
+// new Media('../assets/boxcovers/iron_man.jpg', "Iron Man", "2008"),
+// new Media('../assets/boxcovers/les_mis.jpg', "Les Miserables", "2012"),
+// new Media('../assets/boxcovers/lorax.jpg', "The Lorax", "2012"),
+// new Media('../assets/boxcovers/man_of_steel_large.jpg', "Man of Steel", "2013"),
+// new Media('../assets/boxcovers/monsters_inc.jpg', "Monsters Inc", "2001"),
+// new Media('../assets/boxcovers/new_moon.jpg', "The Twilight Saga: New Moon", "2009"),
+// new Media('../assets/boxcovers/simpsons_movie_large.jpg', "The Simpsons Movie", "2007"),
+// new Media('../assets/boxcovers/skyfall_large.jpg', "Skyfall", "2012"),
+// new Media('../assets/boxcovers/spider_man.jpg', "Spider-Man", "2012"),
+// new Media('../assets/boxcovers/star_trek.jpg', "Star Trek", "2009"),
+// new Media('../assets/boxcovers/train_dragon.jpg', "How to Train Your Dragon", "2010"),
+// new Media('../assets/boxcovers/wall_e.jpg', "Wall-E", "2008"),
+// new Media('../assets/boxcovers/wreck_it_ralph.jpg', "Wreck-It Ralph", "2012"),
+  ];
+}, // end of makeMovieObjects function
 
 
 // INTRO ANIMATION
@@ -84,32 +85,49 @@ return table;
 // BUILDING MENUS
 
 
-function init(table) {
-
-//console.log(table[0].genre);
+init: function(){
 
     var genresList = [];
-    var aToZsList = [];
-    var imdbRatingsList = [];
+    function sortByGenre() {
+        // scoop up genres & create a uniq sorted array
+        for (var i = 0; i < module.table.length; i++) {
+            var genreCandidatesArray = module.table[i].genre.split(", ");
+            genresList = $.merge(genresList, genreCandidatesArray);
+        }
+        console.log(genresList);
+        genresList = _.uniq(genresList);
+        console.log(genresList);
+        genresList.sort();
+        console.log(genresList);
+        // create div for each genre type & display movies
+        for (var j = 0; j < genresList.length; j++) {
+            var genreDiv = document.createElement('div');
+            var moviesByGenreDiv = document.createElement('div');
+            genreDiv.className = "genre";
+            document.getElementsByClassName("selections")[0].appendChild(genreDiv);
+            genreDiv.innerHTML = genresList[j];
+            genreDiv.appendChild(moviesByGenreDiv);
+            // for (var k = 0[ k < module.table.length; k++]) {
+            //     if (module.table[k].genre) {
 
-    // for (var i = 0; i < table.length; i++) {
+            //     }
 
-    //     console.log(table[i].title);
-    //     console.log(table[i].year);
-        // var genreCandidatesArray = table[i].genre.split("");
-        // console.log(table[i].genre);
+            // }
 
-        // for (var j = 0; j < genreCandidatesArray.length; j++) {
-        //     if (genreCandidatesArray[j] in genresList === false) {
-        //         genresList.push(genreCandidatesArray[j]);
-        //     }
-        //     if (genreCandidatesArray[j] in genresList) {
-        //         console.log("whoo");
-        //     }
-        // }
-    // aToZsList.push(table[i].genre)
-    // imdbRatingsList.push(table[i].)
-}
+        }
+    }
+
+            //  for (var j = 0; j < genreCandidatesArray.length; j++) {
+            //      if (genreCandidatesArray[j] in genresList === false) {
+            //          genresList.push(genreCandidatesArray[j]);
+            //      }
+            //      if (genreCandidatesArray[j] in genresList) {
+            //          console.log("whoo");
+            //      }
+            //  }
+            // ZsList.push(table[i].genre)
+            // bRatingsList.push(table[i].)
+    // }
 
 // console.log(genresList);
     // DEFAULT: sort by genre
@@ -123,9 +141,9 @@ function init(table) {
 //         var home = document.getElementsByClassName("selections");
 //         home[0].appendChild(movieImg);
 //     }
-// } //end of init function
-
-makeMovieObjects(init(table));
+  } //end of init function
+}
+module.makeMovieObjects();
 }
 vodApp();
 
--- v3/menu3.js
+++ v3/menu3.js
@@ -20,28 +20,28 @@ Media.prototype.plot = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 
 // create movie objects && put movie objects into table
 var table = [
-    new Media('../assets/boxcovers/avengers.jpg', "Avengers", "2012", 7, "PG-13"),
-    new Media('../assets/boxcovers/blade_runner.jpg', "Blade Runner", "1982", 8, "R"),
-    new Media('../assets/boxcovers/brave.jpg', "Brave", "2012", 8, "G"),
-    new Media('../assets/boxcovers/catching_fire.jpg', "The Hunger Games: Catching Fire", "2013", 6, "PG-13"),
-    new Media('../assets/boxcovers/django.jpg', "Django Unchained", "2012", 9, "R"),
-    new Media('../assets/boxcovers/finding_nemo.jpg', "Finding Nemo", "2003", 9, "G"),
-    new Media('../assets/boxcovers/hobbit.jpg', "The Hobbit: An Unexpected Journey", "2012", 9, "PG"),
-    new Media('../assets/boxcovers/hotel_transylvania.jpg', "Hotel Transylvania", "2012", 5, "G"),
-    new Media('../assets/boxcovers/hugo.jpg', "Hugo", "2011", 9, "G"),
-    new Media('../assets/boxcovers/iron_man.jpg', "Iron Man", "2008", 8, "PG-13"),
-    new Media('../assets/boxcovers/les_mis.jpg', "Les Miserables", "2012", 7, "PG"),
-    new Media('../assets/boxcovers/lorax.jpg', "The Lorax", "2012", 7, "G"),
-    new Media('../assets/boxcovers/man_of_steel_large.jpg', "Man of Steel", "2013", 6, "PG-13"),
-    new Media('../assets/boxcovers/monsters_inc.jpg', "Monsters Inc", "2001", 9, "G"),
-    new Media('../assets/boxcovers/new_moon.jpg', "The Twilight Saga: New Moon", "2009", 6, "PG-13"),
-    new Media('../assets/boxcovers/simpsons_movie_large.jpg', "The Simpsons Movie", "2007", 8, "PG"),
-    new Media('../assets/boxcovers/skyfall_large.jpg', "Skyfall", "2012", 6, "G"),
-    new Media('../assets/boxcovers/spider_man.jpg', "Spider-Man", "2012", 7, "PG"),
-    new Media('../assets/boxcovers/star_trek.jpg', "Star Trek", "2009", 8, "PG-13"),
-    new Media('../assets/boxcovers/train_dragon.jpg', "How to Train Your Dragon", "2010", 7, "G"),
-    new Media('../assets/boxcovers/wall_e.jpg', "Wall-E", "2008", 9, "G"),
-    new Media('../assets/boxcovers/wreck_it_ralph.jpg', "Wreck-It Ralph", "2012", 7, "G"),
+new Media('../assets/boxcovers/avengers.jpg', "Avengers", "2012", "Action / Adventure", 7, "PG-13"),
+new Media('../assets/boxcovers/blade_runner.jpg', "Blade Runner", "1982", "Drama", 8, "R"),
+new Media('../assets/boxcovers/brave.jpg', "Brave", "2012", "Family", 8, "G"),
+new Media('../assets/boxcovers/django.jpg', "Django Unchained", "2012", "Drama", 9, "R"),
+new Media('../assets/boxcovers/finding_nemo.jpg', "Finding Nemo", "2003", "Family", 9, "G"),
+new Media('../assets/boxcovers/hobbit.jpg', "Hobbit: An Unexpected Journey", "2012", "Family", 9, "PG"),
+new Media('../assets/boxcovers/hotel_transylvania.jpg', "Hotel Transylvania", "2012", "Family", 5, "G"),
+new Media('../assets/boxcovers/train_dragon.jpg', "How to Train Your Dragon", "2010", "Family", 7, "G"),
+new Media('../assets/boxcovers/hugo.jpg', "Hugo", "2011", "Family", 9, "G"),
+new Media('../assets/boxcovers/catching_fire.jpg', "Hunger Games: Catching Fire", "2013", "Drama", 6, "PG-13"),
+new Media('../assets/boxcovers/iron_man.jpg', "Iron Man", "2008", "Action / Adventure", 8, "PG-13"),
+new Media('../assets/boxcovers/les_mis.jpg', "Les Miserables", "2012", "Drama", 7, "PG"),
+new Media('../assets/boxcovers/lorax.jpg', "Lorax", "2012", "Family", 7, "G"),
+new Media('../assets/boxcovers/man_of_steel_large.jpg', "Man of Steel", "2013", "Action / Adventure", 6, "PG-13"),
+new Media('../assets/boxcovers/monsters_inc.jpg', "Monsters Inc", "2001", "Family", 9, "G"),
+new Media('../assets/boxcovers/simpsons_movie_large.jpg', "Simpsons Movie", "2007", "Family", 8, "PG"),
+new Media('../assets/boxcovers/skyfall_large.jpg', "Skyfall", "2012", "Action / Adventure", 6, "G"),
+new Media('../assets/boxcovers/spider_man.jpg', "Spider-Man", "2012", "Action / Adventure", 7, "PG"),
+new Media('../assets/boxcovers/star_trek.jpg', "Star Trek", "2009", "Drama", 8, "PG-13"),
+new Media('../assets/boxcovers/new_moon.jpg', "Twilight Saga: New Moon", "2009", "Drama", 6, "PG-13"),
+new Media('../assets/boxcovers/wall_e.jpg', "Wall-E", "2008", "Family", 9, "G"),
+new Media('../assets/boxcovers/wreck_it_ralph.jpg', "Wreck-It Ralph", "2012", "Family", 7, "G"),
 ];
 
 
@@ -52,30 +52,94 @@ var table = [
 
 // BUILDING MENUS
 
-function buildMenus(table) {
 
-    var genresList = [];
-    var aToZsList = [];
-    var imdbRatingsList = [];
 
-    // for (var i = 0; i < table.length; i++) {
-
-    //     console.log(table[i].title);
-    //     console.log(table[i].year);
-        // var genreCandidatesArray = table[i].genre.split("");
-        // console.log(table[i].genre);
-
-        // for (var j = 0; j < genreCandidatesArray.length; j++) {
-        //     if (genreCandidatesArray[j] in genresList === false) {
-        //         genresList.push(genreCandidatesArray[j]);
-        //     }
-        //     if (genreCandidatesArray[j] in genresList) {
-        //         console.log("whoo");
-        //     }
-        // }
-    // aToZsList.push(table[i].genre)
-    // imdbRatingsList.push(table[i].)
+function clear(){
+    $(".propertyTitle").remove();
+    $(".moviesByPropertyDiv").remove();
 }
+
+function displayByGenre() {
+    // sort movie results chronologically w/ newest first
+    function sortByYear(a, b){
+      if (a.year < b.year) return 1;
+      if (a.year > b.year) return -1;
+      return 0;
+  }
+  table.sort(sortByYear);
+  // start fresh if user switches to this menu
+  clear();
+    // generate an array of available genres
+    var genresList = [];
+    for (var i = 0; i < table.length; i++) {
+        genresList.push(table[i].genre);
+    }
+    genresList = _.uniq(genresList);
+    // create a div for each genre && add a 'new' div on top
+    
+    for (var j = 0; j < genresList.length; j++) {
+        var propertyDiv = document.createElement('div');
+        var moviesByPropertyDiv = document.createElement('div');
+        propertyDiv.className = "propertyTitle";
+        document.getElementsByClassName("selections")[0].appendChild(propertyDiv);
+        propertyDiv.innerHTML = genresList[j];
+        propertyDiv.appendChild(moviesByPropertyDiv);
+        moviesByPropertyDiv.className = "moviesByPropertyDiv";
+        // populate boxCovers beneath each genre title div
+        for (var k = 0; k < table.length; k++) {
+            if (table[k].genre === genresList[j]) {
+                var movieImg = document.createElement('img');
+                movieImg.src = table[k].boxCover;
+                movieImg.id = table[k].name;
+                movieImg.className = "movieImg";
+                moviesByPropertyDiv.appendChild(movieImg);
+            }
+
+        }
+    }
+}
+//displayByGenre();
+
+function displayByAtoZ() {
+    // start fresh when user switches to this menu
+    clear();
+    var aToZ = ["A-G", "H-P", "Q-Z"];
+
+    for (var j = 0; j < aToZ.length; j++) {
+        var propertyDiv = document.createElement('div');
+        var moviesByPropertyDiv = document.createElement('div');
+        propertyDiv.className = "propertyTitle";
+        document.getElementsByClassName("selections")[0].appendChild(propertyDiv);
+        propertyDiv.innerHTML = aToZ[j];
+        propertyDiv.appendChild(moviesByPropertyDiv);
+        moviesByPropertyDiv.className = "moviesByPropertyDiv";
+        // populate boxCovers beneath each genre title div
+
+        
+        for (var k = 0; k < table.length; k++) {
+            var movieFirstLetter = table[k].title.split("")[0];
+            var categoryFirstLetter = aToZ[j].split("")[0];
+            var categoryLastLetter = aToZ[j].split("")[2];
+            if (movieFirstLetter >= categoryFirstLetter && movieFirstLetter <= categoryLastLetter) {
+                var movieImg = document.createElement('img');
+                movieImg.src = table[k].boxCover;
+                movieImg.id = table[k].name;
+                movieImg.className = "movieImg";
+                moviesByPropertyDiv.appendChild(movieImg);
+            }
+        }
+    }
+
+
+
+}
+//displayByAtoZ();
+
+
+function displayByPopularity() {
+
+}
+
 
 // console.log(genresList);
     // DEFAULT: sort by genre
